72ed36a5a70d37d50d5443db72439fe57a9509d0
ironm102 <yunis.albreihi@gmail.com>
2021-01-21 14:24:54 +0100

    Added animation to planets. Fixed stack overflow.

--- FGMath_GroupAss/Assets/Scripts/YunisOrbit.cs
+++ FGMath_GroupAss/Assets/Scripts/YunisOrbit.cs
@@ -10,6 +10,7 @@ public class YunisOrbit : MonoBehaviour
     {
         public GameObject m_body = null;
         public List<Planet> m_moons = new List<Planet>();
+        public Transform m_parent = null;
         public float m_speed = 0.0f;
         public float m_radius = 0.0f;
         public float m_angle = 0.0f;
@@ -18,66 +19,70 @@ public class YunisOrbit : MonoBehaviour
     GameObject m_sun = null;
     List<Planet> m_planets = new List<Planet>();
 
-    int m_radius = 10;
+    int m_radius = 20;
 
     private void Awake()
     {
         m_sun = GameObject.CreatePrimitive(PrimitiveType.Sphere);
         m_sun.transform.parent = transform;
         m_sun.name = "Sun";
+        m_sun.transform.localScale = new Vector3(3, 3, 3);
 
-        int t_numberOfPlanets = Random.Range(1, 10);
+        int t_numberOfPlanets = Random.Range(5, 10);
 
         CreatePlanets(t_numberOfPlanets);
     }
 
     private void Update()
     {
-        //foreach (var t_planet in m_planets)
-        //{
-        //    MovePlanet(t_planet, m_sun.transform.position);
-        //}
+        foreach (var t_planet in m_planets)
+        {
+            MovePlanet(t_planet);
+        }
     }
 
-    void MovePlanet(Planet planet, Vector3 center)
+    void MovePlanet(Planet planet)
     {
         planet.m_angle += Time.deltaTime * planet.m_speed;
-        planet.m_body.transform.position = getPositionInRadius(center, planet.m_radius, planet.m_angle);
+        planet.m_body.transform.position = getPositionInRadius(planet.m_parent.position, planet.m_radius, planet.m_angle);
 
         foreach (var t_moon in planet.m_moons)
         {
-            MovePlanet(planet, planet.m_body.transform.position);
-        } 
+            MovePlanet(t_moon);
+        }
     }
 
     void CreatePlanets(int numPlanets)
     {
         for (int i = 0; i < numPlanets; ++i)
         {
-            Planet t_planet = CreatePlanet(0.75f, Random.Range(0, 4), m_radius);
+            Planet t_planet = CreatePlanet(m_sun.transform, 0.75f, m_sun.transform.position, Random.Range(1, 4), new Vector2(4, m_radius), "Planet");
             m_planets.Add(t_planet);
         }
     }
 
-    Planet CreatePlanet(float planetSize, int numMoons, int radius)
+    Planet CreatePlanet(Transform parent, float planetSize, Vector3 center, int numMoons, Vector2 radiusRange, string name)
     {
         GameObject t_body = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        t_body.name = name;
+        t_body.transform.parent = parent;
 
-        int t_randPosX = Random.Range(2, radius);
+        float t_randPosX = Random.Range(radiusRange.x, radiusRange.y);
         float t_randAngle = Random.Range(0, 360);
 
         t_body.transform.position = new Vector3(t_randPosX, 0.0f, 0.0f);
-        t_body.transform.position = getPositionInRadius(m_sun.transform.position, t_randPosX, t_randAngle);
+        t_body.transform.position = getPositionInRadius(center, t_randPosX, t_randAngle);
         t_body.transform.localScale *= planetSize;
 
         Planet t_planet = new Planet();
         t_planet.m_body = t_body;
+        t_planet.m_parent = parent;
         t_planet.m_speed = Random.Range(1, 20);
         t_planet.m_radius = t_randPosX;
         t_planet.m_angle = t_randAngle;
         for (int i = 0; i < numMoons; i++)
         {
-            t_planet.m_moons.Add(CreatePlanet(Random.Range(0.05f, planetSize / 0.5f), 0, 0));
+            t_planet.m_moons.Add(CreatePlanet(t_body.transform, Random.Range(0.05f, planetSize / 2.0f), t_body.transform.position, 0, new Vector2(1, 3), "Moon"));
         }
         return t_planet;
     }
@@ -89,12 +94,4 @@ public class YunisOrbit : MonoBehaviour
 
         return new Vector3(t_posX, 0.0f, t_posZ);
     }
-
-    //private void OnDrawGizmos()
-    //{
-    //    foreach (var t_planet in m_planets)
-    //    {
-    //        Gizmos.DrawWireSphere(m_sun.transform.position, t_planet.m_radius);
-    //    }
-    //}
 }
